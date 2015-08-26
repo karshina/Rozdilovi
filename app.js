e4c121e43ad6d260033a6ecc06790c2575f2dfb0
Aliya Karshina <aliya.karshina@gmail.com>
2015-08-26 23:00:05 +0300

    use html5boilerplate

--- app.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-var app = connect();
-var port = 3333;
-
-app.use(serveStatic(__dirname, {'index': ['index.html']}));
-app.listen(port);
-console.log('> listen post:', port);
