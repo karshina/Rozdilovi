2048da499fa4b5e7c49c29d4913ebc0cdf9700f5
Yuriy Bogdanov <chinsay@gmail.com>
2015-08-30 14:55:10 +0300

    draw card using canvas

--- js/video.js
+++ js/video.js
@@ -4,13 +4,15 @@ $(document).ready(function($) {
   var $play = $('#play')
   var $body = $('body')
   var $content = $('.video-content')
-  var $card = $('.video-content-main')
+  var $card = $('#card')
   var $playerContent = $('.player-content')
   var $cross = $('.cross')
   var $simbol = $('.simbol')
 
   var player = {}
   var track
+
+  var card = Card($card[0])
 
   $window.resize(function() {
     if (!player.h) return
@@ -36,12 +38,11 @@ $(document).ready(function($) {
             $content.addClass('none')
           }
           else if (e.data == YT.PlayerState.PAUSED) {
-            var $span = $('.video-content-span')
             var text = track.text.reduce(function (res, cur) {
               return player.getCurrentTime() > cur.time ? cur : res
             }, {})
-            $span.html(text.words || '')
             $content.removeClass('none')
+            card.draw(text.words || '')
           }
           else if (e.data == YT.PlayerState.ENDED) {
             closeIframe()
@@ -76,10 +77,9 @@ $(document).ready(function($) {
   })
 
   $card.on('click', function (event) {
-    var simbol = $('.simbol.active').html()
-    var style = "left: " + event.offsetX + "px; top: " + event.offsetY + "px"
+    var simbol = $('.simbol.active').text()
     if (!simbol) return
-    $(this).append( "<span class='card-simbol' style='"+style+"'>"+simbol+"</span>")
+    card.addMark(simbol, event.offsetX, event.offsetY)
   })
 
   function closeIframe() {
@@ -90,12 +90,96 @@ $(document).ready(function($) {
     $playerContent.html('<div id="player"></div>')
     $container.addClass('none')
     $content.addClass('none')
-    $card.html('<span class="video-content-span"></span>')
+    card.reset()
+  }
+
+  // Card Class
+  // receives DOM element of canvas, like so: var card = Card(document.getElementById('card'))
+  function Card(card) {
+    var ctx = card.getContext('2d'),
+        isRetina = isHighDensity(),
+        width = card.width,
+        height = card.height,
+        markCorrectionX = 0,
+        markCorrectionY = 7
+
+    if (isRetina) {
+      card.style.width = width + "px"
+      card.style.height = height + "px"
+      card.width = card.width * 2
+      card.height = card.height * 2
+      ctx.scale(2,2)
+    }
+
+    function isHighDensity(){
+      return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 124dpi), only screen and (min-resolution: 1.3dppx), only screen and (min-resolution: 48.8dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (min-device-pixel-ratio: 1.3)').matches)) || (window.devicePixelRatio && window.devicePixelRatio > 1.3));
+    }
+
+    function canvasWrapText(ctx, text, x, y, maxWidth, lineHeight) {
+      var words = text.split(' '),
+          line = '',
+          lines = []
+
+      y += lineHeight
+
+      for (var n = 0, len = words.length; n < len; n++) {
+        var testLine = line + words[n] + ' ',
+            metrics = ctx.measureText(testLine),
+            testWidth = metrics.width
+
+        if (testWidth > maxWidth) {
+          lines.push({ text: line, x: x, y: y })
+          line = words[n] + ' '
+          y += lineHeight
+        } else {
+          line = testLine
+        }
+      }
+
+      lines.push({ text: line, x: x, y: y })
+      return lines
+    }
+
+    function draw(text) {
+      reset()
+
+      var bg = new Image()
+      bg.src = 'img/stopblock.png'
+      bg.onload = function(){
+        ctx.drawImage(this, 0, 0, this.width, this.height)
+
+        ctx.font = "300 30px FranklinGothicBook"
+        ctx.fillStyle = '#908d8d'
+
+        var paddingTop = 30
+        var paddingSides = 30
+        var txt = canvasWrapText(ctx, text, paddingSides, paddingTop, width - paddingSides, 35)
+        for (var i = 0; i < txt.length; i++){
+          ctx.fillText(txt[i].text, txt[i].x, txt[i].y)
+        }
+      }
+    }
+
+    function addMark(text, x, y) {
+      ctx.fillStyle = '#2C8E31'
+      ctx.fillText(text, x + markCorrectionX, y + markCorrectionY)
+    }
+
+    function reset() {
+      ctx.clearRect(0, 0, width, height)
+    }
+
+    return {
+      draw: draw,
+      addMark: addMark,
+      reset: reset
+    }
   }
 
   // For quick access to card development
   if (document.location.hash == "#video-content-dev") {
     $container.removeClass('none')
     $content.removeClass('none')
+    card.draw("хочеться говорити тихо, щоби тебе ніхто не почув, а почувши – не зрозумів")
   }
 })
