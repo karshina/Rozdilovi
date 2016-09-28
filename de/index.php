c4f7d1602ece20cdc335dee73051b64dfe89a61f
Dmitry Leader <dmitry.leader@gmail.com>
2016-09-28 13:07:53 +0300

    cards shared without meta-text

--- de/index.php
+++ de/index.php
@@ -11,12 +11,16 @@ $index = file_get_contents(__DIR__ . '/index.html');
 $index = str_replace('{{URL}}', htmlspecialchars(full_url($_SERVER, true)), $index);
 
 if (isset($_GET['img'])) {
+  $index = str_replace('{{DESC}}', '', $index);
+  $index = str_replace('{{TITLE}}', '||', $index);
   $uploadFilename = 'uploads/' . preg_replace('/[^a-z\d]/i', '', $_GET['img']) . '.jpg';
   if (file_exists(dirname(__DIR__) . '/' . $uploadFilename)) {
     $index = str_replace("{{IMAGE}}", url_origin($_SERVER, true) . '/' . $uploadFilename, $index);
   }
 } else {
   $index = str_replace('{{IMAGE}}', url_origin($_SERVER, true) . '/' . 'img/og_image_de.jpg', $index);
+  $index = str_replace('{{DESC}}', 'rozdILovI ist ein multidisziplinäre Kunstprojekt von ArtPole / Ukraine. Тext / Stimme: Serhij Zhadan, Іdee / Visualisierung: Olja Mychajljuk, Мusik: Aleksej Vorsoba / Vlad Krejmer.', $index);
+  $index = str_replace('{{TITLE}}', 'rozdILovI', $index);
 }
 
 echo $index;
--- en/index.php
+++ en/index.php
@@ -11,12 +11,16 @@ $index = file_get_contents(__DIR__ . '/index.html');
 $index = str_replace('{{URL}}', htmlspecialchars(full_url($_SERVER, true)), $index);
 
 if (isset($_GET['img'])) {
+  $index = str_replace('{{DESC}}', '', $index);
+  $index = str_replace('{{TITLE}}', '||', $index);
   $uploadFilename = 'uploads/' . preg_replace('/[^a-z\d]/i', '', $_GET['img']) . '.jpg';
   if (file_exists(dirname(__DIR__) . '/' . $uploadFilename)) {
     $index = str_replace("{{IMAGE}}", url_origin($_SERVER, true) . '/' . $uploadFilename, $index);
   }
 } else {
   $index = str_replace('{{IMAGE}}', url_origin($_SERVER, true) . '/' . 'img/og_image_en.jpg', $index);
+  $index = str_replace('{{DESC}}', 'rozdILovI is an interdisciplinary art project by ArtPole Agency / Ukraine. Text and voice: Serhiy Zhadan, idea and visualization: Olya Mykhailyuk, music: Aleksey Vorsoba and Vlad Kreymer.', $index);
+  $index = str_replace('{{TITLE}}', 'rozdILovI', $index);
 }
 
 echo $index;
