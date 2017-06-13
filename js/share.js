b17ea44db650ec405feafab558f210ac035efba4
Dmitry Leader <dmitry.leader@gmail.com>
2017-06-13 18:37:01 +0300

    misc fixes

--- js/share.js
+++ js/share.js
@@ -6,15 +6,18 @@ $(document).ready(function($) {
 
         $('.share-content').addClass('none');
         var contentId = $tab.data('contentId');
-        $('#' + contentId).removeClass('none');
 
-        var top_of_element = $('#' + contentId).offset().top;
-    	var bottom_of_element = $('#' + contentId).offset().top + $('#' + contentId).outerHeight();
-    	var bottom_of_screen = $(window).scrollTop() + $(window).height();
-    	//var top_of_screen = $(window).scrollTop();
+        if (contentId) {
+            $('#' + contentId).removeClass('none');
 
-    	if(bottom_of_screen < bottom_of_element)
-    		$("html, body").animate({ scrollTop: top_of_element-100}, 400);
+            var top_of_element = $('#' + contentId).offset().top;
+            var bottom_of_element = $('#' + contentId).offset().top + $('#' + contentId).outerHeight();
+    	    var bottom_of_screen = $(window).scrollTop() + $(window).height();
+            //var top_of_screen = $(window).scrollTop();
+
+            if(bottom_of_screen < bottom_of_element)
+            $("html, body").animate({ scrollTop: top_of_element-100}, 400);
+        }
 
     	$('.share-content').children('.success').addClass('none');
     	$('.throw-error').addClass('none');
